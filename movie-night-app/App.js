faa99821048863262d499cc018b54467e9385323
Samta Priya Jain <priyaj127@hotmail.com>
2021-01-16 16:52:30 -0500

    Navigation WIP

--- movie-night-app/App.js
+++ movie-night-app/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import MovieCard from './components/MovieCard';
+import GroupInfo from './screens/GroupInfo';
 
-export default function App() {
+function MovieCardTest() {
   return (
     <View style={styles.container}>
-      <MovieCard 
+      <MovieCard
         title="Up"
         year="2009"
         summary="Lots and lots and lots and lots and lots and lots and lots and lots and lots and lots and lots of balloons."
@@ -14,8 +18,22 @@ export default function App() {
         genres={["Cartoon", "Adventure"]}
       />
     </View>
+  )
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="GroupInfo">
+        <Stack.Screen name="GroupInfo" component={GroupInfo} />
+        <Stack.Screen name="MovieCard" component={MovieCardTest}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+const Stack = createStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
---
+++ movie-night-app/screens/GroupInfo.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+export default function GroupInfo({navigation}) {
+    return (
+      <View style={styles.container}>
+        <TextInput mode={"outlined"} placeholder={"Input your group code"} style={styles.input}>
+        </TextInput>
+        <Button style={styles.submit} dark={true} uppercase={true} mode="contained" onPress={navigation.navigate('MovieCard')}>
+          Submit
+        </Button>
+      </View>
+    );
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    maxHeight: 24,
+    marginBottom: 120,
+  },
+  submit: {
+    backgroundColor: "#7B59D9",
+    width: 120,
+    height: 44,
+    justifyContent: "center",
+  },
+});
