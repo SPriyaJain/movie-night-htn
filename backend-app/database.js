806a8c043b429b0985a799ad84f25ef638b3f4a4
Justin Reiter <JDReiter@uwaterloo.ca>
2021-01-16 15:00:49 -0500

    Refactor db creation to separate file

---
+++ backend-app/database.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const pg = require('pg')
+
+const config = {
+    user: 'htnuser',
+    host: 'free-tier.gcp-us-central1.cockroachlabs.cloud',
+    database: 'sour-tapir-131.defaultdb',
+    password: 'PASSWORDGOESHERE',
+    port: 26257,
+    ssl: {
+        ca: fs.readFileSync('./cc-ca.crt').toString()
+    }
+};
+
+var db = new pg.Pool(config);
+module.exports = db;
