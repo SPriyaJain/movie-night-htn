e14443ea7616da227eab3b3cbcf7c8fa3406a835
Victor Sun <victorjsun@gmail.com>
2021-01-16 10:45:22 -0500

    Add config to deploy to App Engine

--- backend-app/app.js
+++ backend-app/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const fs = require('fs')
 const pg = require('pg')
+const cors = require('cors')
 const app = express()
-const port = 3001
+
+app.use(cors())
+const port = 8080
 
 const config = {
   user: 'htnuser',
@@ -24,6 +27,19 @@ app.get('/', (req, res) => {
       res.status(500).send(err);
     } else {
       res.status(200).send("It is now "+response.rows[0]["now"]);
+    }
+    pool.end()
+  })
+})
+
+app.get('/test', (req, res) => {
+  const pool = new pg.Pool(config);
+  
+  pool.query('SELECT 1', (err, response) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send([{"title": "Boo"}, {"title": "Boo2"}]);
     }
     pool.end()
   })
