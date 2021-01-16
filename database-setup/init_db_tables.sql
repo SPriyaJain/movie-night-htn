0365595c2fb025c3597064135b0dbf0b9273b3b9
Justin Reiter <JDReiter@uwaterloo.ca>
2021-01-16 16:39:30 -0500

    Modify table schemas for serial values

--- database-setup/init_db_tables.sql
+++ database-setup/init_db_tables.sql
@@ -27,27 +27,36 @@ create table movie_genres (
 );
 
 create table users (
-    uid integer primary key not null,
+    uid serial primary key not null,
     name VARCHAR(255) not null,
     joined_at timestamp without time zone not null
 );
 
-create table liked_movies (
+create table user_liked_genres (
+    uid serial not null,
+    genre_id integer not null,
+    primary key(uid, genre_id),
+    foreign key(uid) references users(uid),
+    foreign key(genre_id) references genres(genre_id)
+);
+
+create table user_liked_movies (
     mid integer not null,
-    uid integer not null,
+    uid serial not null,
+    is_liked boolean not null,
     primary key(mid, uid),
     foreign key(mid) references movies(mid),
     foreign key(uid) references users(uid)
 );
 
 create table groups (
-    group_id integer primary key not null,
+    group_id serial primary key not null,
     pin varchar(4) not null
 );
 
 create table group_members (
-    group_id integer not null,
-    uid integer not null,
+    group_id serial not null,
+    uid serial not null,
     primary key(group_id, uid),
     foreign key(group_id) references groups(group_id),
     foreign key(uid) references users(uid)
