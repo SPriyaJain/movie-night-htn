bdf33fbff6aaff5df25633238f853aeec32bf489
Justin Reiter <JDReiter@uwaterloo.ca>
2021-01-16 01:51:57 -0500

    Initial commit for database setup scripts

---
+++ database-setup/README.md
@@ -0,0 +1,12 @@
+# database-setup
+Info on setting up the databases with movie data.
+
+### init_db_tables.sql
+* Script for initializing the database tables (CockroachDB)
+
+### clean_movie_metadata.py
+* Script to clean movie CSVs into SQL scripts to insert into databases
+
+## Sources
+* [Movies Metadata (Kaggle)](https://www.kaggle.com/rounakbanik/the-movies-dataset?select=movies_metadata.csv)
+* [Movies on Streaming Platforms (Kaggle)](https://www.kaggle.com/ruchi798/movies-on-netflix-prime-video-hulu-and-disney)
---
+++ database-setup/init_db_tables.sql
@@ -0,0 +1,54 @@
+
+create table movies (
+    mid integer primary key not null,
+    name VARCHAR(255) not null,
+    on_netflix boolean not null,
+    on_prime boolean not null,
+    on_disney boolean not null,
+    on_hulu boolean not null,
+    year integer not null,
+    rating integer not null,
+    overview varchar(1023),
+    imdb_id varchar(10) not null,
+    poster_path varchar(255),
+);
+
+create table genres (
+    gid integer primary key not null,
+    name varchar(255) not null
+);
+
+create table movie_genres (
+    mid integer not null,
+    genre_id integer not null,
+    primary key(mid, gid),
+    foreign key(mid) references movies(mid),
+    foreign key(gid) references genres(gid),
+);
+
+create table users (
+    uid integer primary key not null,
+    name VARCHAR(255) not null,
+    joined_at timestamp without time zone not null
+);
+
+create table liked_movies (
+    mid integer not null,
+    uid integer not null,
+    primary key(mid, uid),
+    foreign key(mid) references movies(mid),
+    foreign key(uid) references users(uid),
+);
+
+create table groups (
+    group_id integer primary key not null,
+    pin varchar(4) not null
+);
+
+create table group_members (
+    group_id integer not null,
+    uid integer not null,
+    primary key(group_id, uid),
+    foreign key(group_id) references groups(group_id),
+    foreign key(uid) references users(uid)
+);
