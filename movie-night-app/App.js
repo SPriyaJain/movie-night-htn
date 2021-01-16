6c4e7d0a16cd41d19158d467c0731e30ff4c51e2
Victor Sun <victorjsun@gmail.com>
2021-01-16 12:42:36 -0500

    Add movie gallery

--- movie-night-app/App.js
+++ movie-night-app/App.js
@@ -1,24 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useRef } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MovieCard from './components/MovieCard';
+import MovieGallery from './components/MovieGallery'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MovieCard 
-        title="Up"
-        year="2009"
-        summary="Lots and lots and lots and lots and lots and lots and lots and lots and lots and lots and lots of balloons."
-        photo='https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Up_%282009_film%29.jpg/220px-Up_%282009_film%29.jpg'
-        genres={["Cartoon", "Adventure"]}
-      />
+      <MovieGallery />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-   marginTop: 100,
+   marginTop: 0,
   },
 });
