167911098b8cd6057f407076c8fcb7974c04da6d
Victor Sun <victorjsun@gmail.com>
2021-01-16 10:30:29 -0500

    Deploy app to App Engine

--- backend-app/app.js
+++ backend-app/app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const fs = require('fs')
 const pg = require('pg')
+const cors = require('cors')
 const app = express()
-const port = 3001
+
+app.use(cors())
+const port = 8080
 
 const config = {
   user: 'htnuser',
   host: 'free-tier.gcp-us-central1.cockroachlabs.cloud',
   database: 'sour-tapir-131.defaultdb',
-  password: 'PASSWORDGOESHERE',
+  password: 'uwaterloo123',
   port: 26257,
   ssl: {
     ca: fs.readFileSync('./cc-ca.crt')
@@ -24,6 +27,19 @@ app.get('/', (req, res) => {
       res.status(500).send(err);
     } else {
       res.status(200).send("It is now "+response.rows[0]["now"]);
+    }
+    pool.end()
+  })
+})
+
+app.get('/test', (req, res) => {
+  const pool = new pg.Pool(config);
+  
+  pool.query('SELECT 1', (err, response) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send([{"title": "Boo"}, {"title": "Boo2"}]);
     }
     pool.end()
   })
