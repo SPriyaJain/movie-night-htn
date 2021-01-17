79e2272da037f5711c59f630aafa9a09c049d764
jliaz <juliazhang1108@gmail.com>
2021-01-17 09:39:50 -0500

    can navigate to match screen

--- movie-night-app/App.js
+++ movie-night-app/App.js
@@ -4,12 +4,15 @@ import { StyleSheet, Text, View, Image} from 'react-native';
 import { IconButton, Colors } from 'react-native-paper';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import MovieCard from './components/MovieCard';
 import GroupInfo from './screens/GroupInfo';
 import MatchList from './screens/MatchList';
 import MovieGallery from './components/MovieGallery';
 import MatchedAlert from './components/MatchedAlert';
+
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
 
 
 function MovieCardTest() {
@@ -35,42 +38,16 @@ function MovieCardTest() {
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Matches">
-        <Stack.Screen name="GroupsInfo" component={GroupInfo} 
-          options={{
-            headerStyle: {
-              backgroundColor: '#7B59D9',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-                fontWeight: 'bold',
-            },
-            headerTitle: props => <Image style={styles.logo} source={{uri: 'https://i.imgur.com/VJxRFKi.png'}}/>,
-          }}
-        />
-        <Stack.Screen name="MovieCard" component={MovieCardTest}
-          options={{
-            headerStyle: {
-              backgroundColor: '#7B59D9',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-                fontWeight: 'bold',
-            },
-            headerTitle: props => <Image style={styles.logo} source={{uri: 'https://i.imgur.com/VJxRFKi.png'}}/>,
-            headerBackImage: () => <IconButton icon="cog" width={25} height={25} color={Colors.white} style={styles.navButton}/>,
-            headerBackTitleVisible: false,
-          }}
-        />
-        <Stack.Screen name="Matches" component={MatchList}>
-        </Stack.Screen>
-      </Stack.Navigator>
+    <NavigationContainer style={styles.navContainer}>
+      <Tab.Navigator initialRouteName="GroupsInfo"  activeBackgroundColor='#7B59D9' barStyle={styles.navContainer}>
+        <Tab.Screen name="GroupsInfo" component={GroupInfo} />
+        <Tab.Screen name="MovieCard" component={MovieCardTest} />
+        <Tab.Screen name="Matches" component={MatchList}/>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
-const Stack = createStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
@@ -82,5 +59,8 @@ const styles = StyleSheet.create({
   },
   navButton: {
     color: 'white',
+  },
+  navContainer: {
+    height: 500,
   }
 });
